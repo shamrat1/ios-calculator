46d8d53fbcc2b7f5d61a8988d5a3394508771a01
Yasin Shamrat <yshamrat@gmail.com>
2019-12-01 10:20:03 +0600

    Initial Commit

---
+++ calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by Leads Computer on 12/1/19.
+//  Copyright © 2019 Leads Computer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
